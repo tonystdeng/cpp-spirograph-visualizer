70bfebb0cf3f7f126c46d8e2b1f08d44be674d72
Tony <145881428+tonystdeng@users.noreply.github.com>
2025-01-16 14:55:59 -0500

    Add files via upload

---
+++ make.bash
@@ -0,0 +1,6 @@
+g++ -c ./src/svglib.cpp ./src/compile.cpp ./src/main.cpp ./src/view.cpp 
+g++ ./svglib.o ./compile.o ./main.o ./view.o -o fourierDrawer -lsfml-graphics -lsfml-window -lsfml-system -ltinyxml2
+rm ./svglib.o ./compile.o ./main.o ./view.o
+./fourierDrawer c ./svgs/brain.svg
+#python ./src/compile.py
+./fourierDrawer o ./svgs/brain.svg.dc
