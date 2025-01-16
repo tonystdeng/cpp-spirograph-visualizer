085e0964510f009e236a2fcd024f04bf1a2ff828
Tony <145881428+tonystdeng@users.noreply.github.com>
2025-01-16 14:57:32 -0500

    Update make.bash

--- make.bash
+++ make.bash
@@ -1,5 +1,3 @@
-g++ -c ./src/svglib.cpp ./src/compile.cpp ./src/main.cpp ./src/view.cpp 
-g++ ./svglib.o ./compile.o ./main.o ./view.o -o fourierDrawer -lsfml-graphics -lsfml-window -lsfml-system -ltinyxml2
-rm ./svglib.o ./compile.o ./main.o ./view.o
-./fourierDrawer c ./svgs/brain.svg
-./fourierDrawer o ./svgs/brain.svg.dc
+g++ -c ./src/main.cpp ./src/game.cpp 
+g++ ./main.o ./game.o -o spirograph -lsfml-graphics -lsfml-window -lsfml-system -ltinyxml2
+rm ./main.o ./game.o
